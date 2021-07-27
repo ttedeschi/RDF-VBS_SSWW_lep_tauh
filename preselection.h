56427495a28601568438f73f86f2b89ec94c3556
Tommaso Tedeschi <tedesk95@gmail.com>
2021-07-27 15:21:13 +0200

    Create preselection.h

---
+++ preselection.h
@@ -0,0 +1,13 @@
+/// Header file with functions needed to execute the Python version
+/// preselection step of the analysis. The header is declared to the
+/// ROOT C++ interpreter prior to the start of the analysis via the
+/// `ROOT.gInterpreter.Declare()` function.
+///
+ 
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RVec.hxx"
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "TLatex.h"
+#include "Math/Vector4D.h"
+#include "TStyle.h"
