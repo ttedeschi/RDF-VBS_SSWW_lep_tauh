5e14c5af6f6d516e7287fd0fc952d2fb8fd74cd4
root <tedesk95@gmail.com>
2021-10-21 09:59:50 +0000

    add lepSF to preselection

--- preselection.h
+++ preselection.h
@@ -14,6 +14,12 @@
 #include "Math/Vector4D.h"
 #include "TStyle.h"
 #include <map>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <TH1.h>
+#include <TH2.h>
 
 using namespace ROOT::VecOps;
 using RNode = ROOT::RDF::RNode;
@@ -121,3 +127,298 @@ float GetEventHT(rvec_f Jet_pt, rvec_f Jet_eta, rvec_f Jet_phi, rvec_f Jet_mass)
     }
     return eventSum.Pt();
 }
+
+class WeightCalculatorFromHistogram {
+ public:
+  WeightCalculatorFromHistogram() {}
+  // get the weight from the bin content of the passed histogram
+  WeightCalculatorFromHistogram(TH1 *histogram, bool verbose=false) : histogram_(histogram), verbose_(verbose) {}
+  // get the weight from the bin content of the ratio hist/targethist
+  WeightCalculatorFromHistogram(TH1 *hist, TH1* targethist, bool norm=true, bool fixLargeWeights=true, bool verbose=false);
+  ~WeightCalculatorFromHistogram() {}
+  
+  float getWeight(float x, float y=0) const;
+  float getWeightErr(float x, float y=0) const;
+  
+ private:
+  std::vector<float> loadVals(TH1 *hist, bool norm=true);
+  TH1* ratio(TH1 *hist, TH1* targethist, bool fixLargeWgts);
+  void fixLargeWeights(std::vector<float> &weights, float maxshift=0.0025,float hardmax=3);
+  float checkIntegral(std::vector<float> wgt1, std::vector<float> wgt2);
+
+  TH1* histogram_;
+  std::vector<float> refvals_,targetvals_;
+  bool verbose_;
+  bool norm_;
+};
+
+
+WeightCalculatorFromHistogram::WeightCalculatorFromHistogram(TH1 *hist, TH1* targethist, bool norm, bool fixLargeWeights, bool verbose) {
+  norm_ = norm;
+  verbose_ = verbose;
+  if(hist->GetNcells()!=targethist->GetNcells()) {
+    std::cout << "ERROR! Numerator and denominator histograms have different number of bins!" << std::endl;
+    histogram_=0;
+  } else {
+    for(int i=0; i<(int)hist->GetNcells(); ++i) {
+      refvals_.push_back(hist->GetBinContent(i));
+      targetvals_.push_back(targethist->GetBinContent(i));
+    }
+    histogram_ = ratio(hist,targethist,fixLargeWeights);
+  }
+}
+
+float WeightCalculatorFromHistogram::getWeight(float x, float y) const {
+  if(histogram_==NULL) {
+    std::cout << "ERROR! The weights input histogram is not loaded. Returning weight 0!" << std::endl;
+    return 0.;
+  }
+  if(!histogram_->InheritsFrom("TH2")) {
+    int bin = std::max(1, std::min(histogram_->GetNbinsX(), histogram_->GetXaxis()->FindBin(x)));
+    return histogram_->GetBinContent(bin);
+  } else {
+    int binx = std::max(1, std::min(histogram_->GetNbinsX(), histogram_->GetXaxis()->FindBin(x)));
+    int biny = std::max(1, std::min(histogram_->GetNbinsY(), histogram_->GetYaxis()->FindBin(y)));
+    return histogram_->GetBinContent(binx,biny);
+  }
+}
+
+float WeightCalculatorFromHistogram::getWeightErr(float x, float y) const {
+  if(histogram_==NULL) {
+    std::cout << "ERROR! The weights input histogram is not loaded. Returning weight error 1!" << std::endl;
+    return 1.;
+  }
+  if(!histogram_->InheritsFrom("TH2")) {
+    int bin = std::max(1, std::min(histogram_->GetNbinsX(), histogram_->GetXaxis()->FindBin(x)));
+    return histogram_->GetBinError(bin);
+  } else {
+    int binx = std::max(1, std::min(histogram_->GetNbinsX(), histogram_->GetXaxis()->FindBin(x)));
+    int biny = std::max(1, std::min(histogram_->GetNbinsY(), histogram_->GetYaxis()->FindBin(y)));
+    return histogram_->GetBinError(binx,biny);
+  }
+}
+
+std::vector<float> WeightCalculatorFromHistogram::loadVals(TH1 *hist, bool norm) {
+  int nbins=hist->GetNcells();
+  std::vector<float> vals;
+  for(int i=0; i<nbins; ++i) {
+    double bc=hist->GetBinContent(i);
+    //double val = (i>0 && bc==0 && hist->GetBinContent(i-1)>0 && hist->GetBinContent(i+1)>0) ? 0.5*(hist->GetBinContent(i-1)+hist->GetBinContent(i+1)) : bc;
+    vals.push_back(std::max(bc,0.));
+  }
+  if(verbose_) std::cout << "Normalization of " << hist->GetName() << ": " << hist->Integral() << std::endl;
+  if(norm) {
+    float scale = 1.0/hist->Integral();
+    for(int i=0; i<nbins; ++i) vals[i] *= scale;
+  }
+  return vals;
+}
+
+TH1* WeightCalculatorFromHistogram::ratio(TH1 *hist, TH1* targethist, bool fixLargeWgts) {
+  TH1 *ret = (TH1*)hist->Clone("hweights");
+  ret->SetDirectory(0);
+
+  std::vector<float> vals = loadVals(hist,norm_);
+  std::vector<float> targetvals = loadVals(targethist,norm_);
+  std::vector<float> weights;
+  int nbins = vals.size();
+  if(verbose_) std::cout << "Weights for variable " << hist->GetName() << " with a number of bins equal to " << nbins << ":" << std::endl;
+  for(int i=0; i<nbins; ++i) {
+    float weight = vals[i] !=0 ? targetvals[i]/vals[i] : 1.;
+    if(verbose_) std::cout <<  std::setprecision(3) << weight << " ";
+    weights.push_back(weight);
+  }
+  if(verbose_) std::cout << "." << std::endl;
+  if(fixLargeWgts) fixLargeWeights(weights);
+  if(verbose_) std::cout << "Final weights: " << std::endl;
+  for(int i=0; i<(int)weights.size(); ++i) {
+    ret->SetBinContent(i,weights[i]);
+    if(verbose_) std::cout << std::setprecision(3) << weights[i] << " ";
+  }
+  if(verbose_) std::cout << "." << std::endl;
+  return ret;
+}
+
+float WeightCalculatorFromHistogram::checkIntegral(std::vector<float> wgt1, std::vector<float> wgt2) {
+  float myint=0;
+  float refint=0;
+  for(int i=0; i<(int)wgt1.size(); ++i) {
+    myint += wgt1[i]*refvals_[i];
+    refint += wgt2[i]*refvals_[i];
+  }
+  return (myint-refint)/refint;
+}
+
+void WeightCalculatorFromHistogram::fixLargeWeights(std::vector<float> &weights, float maxshift,float hardmax) {
+  float maxw = std::min(*(std::max_element(weights.begin(),weights.end())),float(5.));
+  std::vector<float> cropped;
+  while (maxw > hardmax) {
+    cropped.clear();  
+    for(int i=0; i<(int)weights.size(); ++i) cropped.push_back(std::min(maxw,weights[i]));
+    float shift = checkIntegral(cropped,weights);
+    if(verbose_) std::cout << "For maximum weight " << maxw << ": integral relative change: " << shift << std::endl;
+    if(fabs(shift) > maxshift) break;
+    maxw *= 0.95;
+  }
+  maxw /= 0.95;
+  if (cropped.size()>0) {
+      for(int i=0; i<(int)weights.size(); ++i) cropped[i] = std::min(maxw,weights[i]);
+      float normshift = checkIntegral(cropped,weights);
+      for(int i=0; i<(int)weights.size(); ++i) weights[i] = cropped[i]*(1-normshift);
+  }
+}
+
+class LeptonEfficiencyCorrector {
+ public:
+
+  LeptonEfficiencyCorrector() {effmaps_.clear();}
+  LeptonEfficiencyCorrector(std::vector<std::string> files, std::vector<std::string> histos);
+  ~LeptonEfficiencyCorrector() {}
+
+  void setLeptons(int nLep, int *lepPdgId, float *lepPt, float *lepEta);
+
+  float getSF(int pdgid, float pt, float eta);
+  float getSFErr(int pdgid, float pt, float eta);
+  const std::vector<float> & run();
+
+private:
+  std::vector<TH2F*> effmaps_;
+  std::vector<float> ret_;
+  int nLep_;
+  float *Lep_eta_, *Lep_pt_;
+  int *Lep_pdgId_;
+};
+
+LeptonEfficiencyCorrector:: LeptonEfficiencyCorrector(std::vector<std::string> files, std::vector<std::string> histos) {
+  effmaps_.clear();
+  if(files.size()!=histos.size()) {
+    std::cout << "ERROR! There should be one histogram per input file! Returning 0 as SF." << std::endl;
+    return;
+  }
+
+  for(int i=0; i<(int)files.size();++i) {
+    TFile *f = TFile::Open(files[i].c_str(),"read");
+    if(!f) {
+      std::cout << "WARNING! File " << files[i] << " cannot be opened. Skipping this scale factor " << std::endl;
+      continue;
+    }
+    TH2F *hist = (TH2F*)(f->Get(histos[i].c_str()))->Clone(("eff_"+histos[i]).c_str());
+    hist->SetDirectory(0);
+    if(!hist) {
+      std::cout << "ERROR! Histogram " << histos[i] << " not in file " << files[i] << ". Not considering this SF. " << std::endl;
+      continue;
+    } else {
+      std::cout << "Loading histogram " << histos[i] << " from file " << files[i] << "... " << std::endl;
+    }
+    effmaps_.push_back(hist);
+    f->Close();
+  }
+}
+
+void LeptonEfficiencyCorrector::setLeptons(int nLep, int *lepPdgId, float *lepPt, float *lepEta) {
+  nLep_ = nLep; Lep_pdgId_ = lepPdgId; Lep_pt_ = lepPt; Lep_eta_ = lepEta;
+}
+
+float LeptonEfficiencyCorrector::getSF(int pdgid, float pt, float eta) {
+  float out=1.;
+  float x = abs(pdgid)==13 ? pt : eta;
+  float y = abs(pdgid)==13 ? fabs(eta) : pt;
+  for(std::vector<TH2F*>::iterator hist=effmaps_.begin(); hist<effmaps_.end(); ++hist) {
+    WeightCalculatorFromHistogram wc(*hist);
+    out *= wc.getWeight(x,y);
+  }
+  return out;
+}
+
+float LeptonEfficiencyCorrector::getSFErr(int pdgid, float pt, float eta) {
+  float out=1.;
+  float x = abs(pdgid)==13 ? pt : eta;
+  float y = abs(pdgid)==13 ? fabs(eta) : pt;
+  for(std::vector<TH2F*>::iterator hist=effmaps_.begin(); hist<effmaps_.end(); ++hist) {
+    WeightCalculatorFromHistogram wc(*hist);
+    out *= wc.getWeightErr(x,y);
+  }
+  return out;
+}
+
+const std::vector<float> & LeptonEfficiencyCorrector::run() {
+  ret_.clear();
+  for (int iL = 0, nL = nLep_; iL < nL; ++iL) {
+    ret_.push_back(getSF((Lep_pdgId_)[iL], (Lep_pt_)[iL], (Lep_eta_)[iL]));
+  }
+  return ret_;
+}
+
+RVec<float> LepSF(rvec_f Electron_pt, rvec_f Electron_eta, rvec_i Electron_pdgId, rvec_f Muon_pt, rvec_f Muon_eta, rvec_i Muon_pdgId,){
+    
+    std::vector<std::string> mu_f, mu_h, el_f, el_h;
+    string path = "leptonSF/"
+    
+    if (muonSelectionTag == "TightWP_2016"){
+        mu_f.push_back(path + "Mu_RunBCDEFGH_SF_ID_2016_syst.root");
+        mu_h.push_back("NUM_TightID_DEN_genTracks_eta_pt");
+    }
+
+    if (electronSelectionTag == "NoIsoMVA90_2016"){
+        el_f.push_back(path + "EGM2D_RECO_SF_2016.root").push_back(path + "2016LegacyReReco_ElectronMVA90noiso_Fall17V2.root");
+        el_h.push_back("EGamma_SF2D").push_back("EGamma_SF2D");
+    }
+
+    if (muonSelectionTag == "TightWP_2017"){
+        mu_f.push_back(path + "Muon_RunBCDEF_SF_ID_2017.root").push_back(path + "Muon_RunBCDEF_SF_ISO_2017.root");
+        mu_h.push_back("NUM_TightID_DEN_genTracks_pt_abseta").push_back("NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta");
+    }
+
+    if (electronSelectionTag == "IsoMVA90_2017"){
+        el_f.push_back(path + "EGM2D_2017_passingRECO_highEt.root").push_back(path + "Electron_MVA90_2017.root");
+        el_h.push_back("EGamma_SF2D").push_back("EGamma_SF2D");
+    }
+
+    if (muonSelectionTag == "TightWP_2018"){
+        mu_f.push_back(path + "Muon_RunABCD_SF_ID_2018.root").push_back(path + "Muon_RunBCDEF_SF_ISO_2017.root");
+        mu_h.push_back("NUM_TightID_DEN_TrackerMuons_pt_abseta").push_back("NUM_TightRelIso_DEN_TightIDandIPCut_pt_abseta");
+    }
+
+    if (electronSelectionTag == "IsoMVA90_2018"){
+        el_f.push_back(path + "EGM2D_passingRECO_2018All.root").push_back(path + "2018_ElectronMVA90Iso.root");
+        el_h.push_back("EGamma_SF2D").push_back("EGamma_SF2D");
+    }
+            
+    worker_mu = LeptonEfficiencyCorrector(self.mu_f, self.mu_h)
+    worker_el = LeptonEfficiencyCorrector(self.el_f, self.el_h)
+    
+    RVec<float> sf_mu(Muon_pt.size());
+    RVec<float> sferr_mu(Muon_pt.size()); 
+    RVec<float> sf_el(Electron_pt.size());
+    RVec<float> sferr_el(Electron_pt.size());
+    
+    for (size_t j = 0; j < Electron_pt.size(); j++) sf_el[j] =  worker_el.getSF(Electron_pdgId[j], Electron_pt[j], Electron_eta[j]);
+    for (size_t j = 0; j < Electron_pt.size(); j++) sferr_el[j] =  worker_el.getSFErr(Electron_pdgId[j], Electron_pt[j], Electron_eta[j]);
+     
+    for (size_t j = 0; j < Muon_pt.size(); j++) sf_mu[j] =  worker_el.getSF(Muon_pdgId[j], Muon_pt[j], Muon_eta[j]);
+    for (size_t j = 0; j < Muon_pt.size(); j++) sferr_mu[j] =  worker_el.getSFErr(Muon_pdgId[j], Muon_pt[j], Muon_eta[j]);
+        
+    RVec<RVec<float>> result;
+    
+    result.push_back(sf_mu);
+    result.push_back(sf_el);
+    
+    RVec<float> Muon_effSF_errUp(Muon_pt.size())
+    for (size_t j = 0; j < Muon_pt.size(); j++) Muon_effSF_errUp[j] = sferr_mu[j] + sf_mu[j];
+    result.push_back(Muon_effSF_errUp);
+    
+    RVec<float> Electron_effSF_errUp(Electron_pt.size())
+    for (size_t j = 0; j < Electron_pt.size(); j++) Electron_effSF_errUp[j] = sferr_el[j] + sf_el[j];
+    result.push_back(Electron_effSF_errUp);
+    
+    RVec<float> Muon_effSF_errDown(Muon_pt.size())
+    for (size_t j = 0; j < Muon_pt.size(); j++) Muon_effSF_errDown[j] = sferr_mu[j] - sf_mu[j];
+    result.push_back(Muon_effSF_errDown);
+    
+    RVec<float> Electron_effSF_errDown(Electron_pt.size())
+    for (size_t j = 0; j < Electron_pt.size(); j++) Electron_effSF_errUp[j] = sferr_el[j] - sf_el[j];
+    result.push_back(Electron_effSF_errDown);
+    
+    return result;
+}
+
